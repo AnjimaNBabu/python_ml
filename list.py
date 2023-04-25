1ad31d10af78b7f685484332cae1541a6c2e8ae7
AnjimaNBabu <anjimanbabu@gmail.com>
2023-04-25 08:54:40 +0530

    odd or even

---
+++ list.py
@@ -0,0 +1,5 @@
+lst=[4,5,6]
+lst2=[9,8]
+k=lst.append(lst2)
+print(k)
+print(lst.index(5))
---
+++ odd or even.py
@@ -0,0 +1,6 @@
+a = int(input('enter the value a:'))
+k=a%2
+if(k==0):
+    print("The Number is Even")
+else:
+    print("The Number is odd")
