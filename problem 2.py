9c87dfbd26797a4a36db7ad1aafcfe53f35b1fd8
AnjimaNBabu <anjimanbabu@gmail.com>
2023-04-26 14:07:38 +0530

    Squaring

---
+++ problem 2.py
@@ -0,0 +1,10 @@
+
+
+#The provided code stb reads an integer,n,from STDIN. For all non-negative integers i<n,print i^2.
+
+
+n = int(input("Enter the Number n:"))
+
+for i in range (n) :
+
+    print (i**2)
