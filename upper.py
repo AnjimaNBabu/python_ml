315907e2f31132e1b3a1be749e3292a975e2c451
AnjimaNBabu <anjimanbabu@gmail.com>
2023-04-26 07:36:02 +0530

    problem1

---
+++ upper.py
@@ -0,0 +1,19 @@
+
+k=""
+name=input()
+for i in name:
+    if i.islower():
+        k+=i.upper()
+    else:
+        k+=i.lower()
+print(k)
+def Swapi(x):
+    x = input("name")
+    if i.islower():
+        k+=i.upper()
+    else:
+        k+=i.lower()
+    print(k)
+name=("")
+ret=Swapi(name)
+print(ret)
